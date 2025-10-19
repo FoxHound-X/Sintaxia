6041aab07b06b333e402692fa18888fb9c704762
FoxHound-X <setiadharma224@gmail.com>
2025-10-19 08:01:42 +0800

    tugas

---
+++ aplikasi/lib/assets/codeppack/paketwarna.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class paketwarna{
+
+  //dark slate color
+  static const darkslate0 = Color(0xff4c566a);
+  static const darkslate1 = Color(0xff434c5e);
+  static const darkslate2 = Color(0xff3b4252);
+  static const darkslate3 = Color(0xff2e3440);
+
+  //Deep Sea
+  static const deepea0 = Color(0xffe0e1dd);
+  static const deepea1 = Color(0xff778da9);
+  static const deepea2 = Color(0xff415a77);
+  static const deepea3 = Color(0xff1b263b);
+  static const deepea4 = Color(0xff0d1b2a);
+}
